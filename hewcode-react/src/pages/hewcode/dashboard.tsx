e41980ea5c172c80d2462ab574721acffce04b78
Sami Mazouz <sychocouldy@gmail.com>
2026-01-09 12:10:01 +0100

    fix

--- hewcode-react/src/pages/hewcode/dashboard.tsx
+++ hewcode-react/src/pages/hewcode/dashboard.tsx
@@ -20,22 +20,24 @@ export default function Dashboard() {
     <PageLayout breadcrumbs={breadcrumbs}>
       <Widgets {...widgets} />
 
-      <div className="mt-4 flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl">
-        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-          <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-            <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+      {widgets.length > 1 && (
+        <div className="mt-4 flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl">
+          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+            <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
+              <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+            </div>
+            <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
+              <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+            </div>
+            <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
+              <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+            </div>
           </div>
-          <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-            <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-          </div>
-          <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
+          <div className="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[45vh] flex-1 overflow-hidden rounded-xl border">
             <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
           </div>
         </div>
-        <div className="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[45vh] flex-1 overflow-hidden rounded-xl border">
-          <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-        </div>
-      </div>
+      )}
     </PageLayout>
   );
 }
